7f4e75a7ac400859bbcaebf89e55348ceba375ea
zengbin <zengbin@126.com>
2019-08-10 09:30:38 +0800

    mit

---
+++ whale/src/views/Index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <my-header></my-header>
+    首页2
+    <my-footer></my-footer>
+  </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style scoped>
+
+</style>
+
